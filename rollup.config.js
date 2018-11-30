3bb99fdeac06d2ac75b4872c79b23c93af168f19
KirillKogtev <kirillkogtev95@gmail.com>
2018-11-30 11:45:40 +0300

    init project with rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript2';
+export default {
+  input: 'src/main.ts',
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+      tsconfigOverride: {
+        compilerOptions: { module: 'es2015', allowJs: 'false' },
+      },
+    }),
+  ],
+  output: {
+    format: 'cjs',
+    file: 'dist/bundle.js',
+    sourcemap: true,
+  }
+};
